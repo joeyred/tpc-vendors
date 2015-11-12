c746b27a45d0f29fba77349d490d9cca3a8cfc3f
Joey Hayes <bjoeyhayes@gmail.com>
2015-11-11 19:42:29 -0500

    set up function structure for markup

---
+++ inc/frontend-functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function tpcvendors_landing_section() {}
+
+function tpcvendors_infobox_section() {}
+
+function tpcvendors_staff_section() {}
+
+function tpcvendors_extra_content_section() {}
