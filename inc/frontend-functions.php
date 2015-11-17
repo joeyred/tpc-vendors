34a5943e58f80f23be3fec3c96d679f2baac5cde
Joey Hayes <bjoeyhayes@gmail.com>
2015-11-17 17:33:06 -0500

    lots of new helper and markup functions

--- inc/frontend-functions.php
+++ inc/frontend-functions.php
@@ -7,18 +7,109 @@
  */
 
 /**
+ * Debugger
+ *
+ * @param  string $function function to call.
+ * @param  string $args     arguments to pass.
+ * @return void
+ */
+function tpcvendors_debugger( $function, $args = '' ) {
+
+	$debug = call_user_func( $function, $args );
+
+	echo $debug;
+}
+
+/**
  * Get image stored via cmb2 file upload
  *
- * @param  string $image_id The field ID of image field.
+ * @param  string $id The field ID of image field.
  * @return string           full image HTML markup
  */
-function tpcvendors_get_image( $image_id ) {
+function tpcvendors_get_image( $id ) {
 
-	$image_id .= '_id';
+	$id .= '_id';
 
-	$image = wp_get_attachment_image( get_post_meta( get_the_ID(), $image_id, 1 ), 'full' );
+	$image = wp_get_attachment_image( get_post_meta( get_the_ID(), $id, 1 ), 'full' );
 
 	return $image;
+}
+
+/**
+ * WYSIWYG helper to process shortcodes and embeds
+ *
+ * @param  string  $content content already retrived from wysiwyg field.
+ * @param  boolean $autop   use auto paragraph function or not.
+ * @return string           fully processed content
+ */
+function tpcvendors_do_wysiwyg_output( $content, $autop = true ) {
+
+	global $wp_embed;
+
+	$content = $wp_embed->autoembed( $content );
+    $content = $wp_embed->run_shortcode( $content );
+    $content = do_shortcode( $content );
+
+    if ( $autop === true ) {
+    	$content = wpautop( $content );
+	}
+
+	return $content;
+}
+
+/**
+ * WYSIWYG helper to retrieve meta and process shortcodes and embeds
+ *
+ * @param  string  $meta_key id of the field.
+ * @param  integer $post_id  post id.
+ * @param  bool    $autop    use auto paragraph function or not.
+ * @return string            fully processed content
+ */
+function tpcvendors_get_wysiwyg_output( $meta_key, $post_id = 0, $autop = true ) {
+
+	global $wp_embed;
+
+    $post_id = $post_id ? $post_id : get_the_id();
+
+    $content = get_post_meta( $post_id, $meta_key, 1 );
+    $content = tpcvendors_do_wysiwyg_output( $content, $autop );
+
+    return $content;
+}
+
+/**
+ * Button builder
+ *
+ * @param  string $id button field id
+ * @return void
+ */
+function tpcvendors_button( $id ) {
+
+	$button_text = get_post_meta( get_the_ID(), $id, true );
+	$button_link = get_post_meta( get_the_ID(), 'vendor_affiliate_link', true );
+
+	if ( isset( $button_text ) ) {
+
+    	$button_text = esc_html( $button_text );
+    }
+    if ( isset( $button_link ) ) {
+
+    	$button_link = esc_html( $button_link );
+    }
+
+	echo '<a href="' . $button_link . '" class="button expand">' . $button_text . '</a>';
+}
+
+/**
+ * Section Title Markup
+ * @param  string $content content to be passed into title.
+ * @return void
+ */
+function tpcvendors_section_title( $content ) {
+
+	echo '<div class="row"><div class="small-12 columns">';
+	echo '<h1 class="section-title">' . $content . '</h1>';
+	echo '</div></div>';
 }
 
 /**
@@ -26,9 +117,9 @@ function tpcvendors_get_image( $image_id ) {
  */
 function tpcvendors_header_logo() {
 
-    $heading_logo = tpcvendors_get_image( 'vendor_header_image' );
+    $header_logo = tpcvendors_get_image( 'vendor_header_image' );
 
-    if ( ! empty($header_logo) ) {
+    if ( ! empty( $header_logo ) ) {
         ?>
         <div class="v-logo-wrap">
 	        <div class="row  collapse">
@@ -47,17 +138,24 @@ function tpcvendors_header_logo() {
  * @param string $media Media Query specific markup.
  */
 function tpcvendors_header_heading( $media ) {
-	$heading = get_post_meta( get_the_ID(), 'vendor_heading_text', true );
+
+	$entry = get_post_meta( get_the_ID(), 'vendor_heading_text', true );
+
+	if ( isset( $entry ) ) {
+
+    	$heading = esc_html( $entry );
+	}
 
     if ( $media === 'medium' ) {
         $markup = '<div class="columns small-12 hide-for-large-up">'
-                . '<h2>' .  $heading . '</h2>'
+                . '<h1>' .  $heading . '</h1>'
                 . '</div>';
     }
+
     if ( $media === 'large' ) {
-        $markup = '<h2 class="show-for-large-up">'
+        $markup = '<h1 class="show-for-large-up">'
                 . $heading
-                . '</h2>';
+                . '</h1>';
     }
 
     if ( ! empty( $heading ) ) {
@@ -73,16 +171,10 @@ function tpcvendors_header_heading( $media ) {
 function tpcvendors_landing_section() {
 
 	$entry = array(
-		'heading' => get_post_meta( get_the_ID(), 'vendor_heading_text', true ),
 		'sub_heading' => get_post_meta( get_the_ID(), 'vendor_subheading_wysiwyg', true ),
-		'header_media' => get_post_meta( get_the_ID(), 'vendor_header_media_wysiwyg', true ),
-		'button_text' => get_post_meta( get_the_ID(), 'vendor_header_button_text', true ),
+		'header_media' => tpcvendors_get_wysiwyg_output( 'vendor_header_media_wysiwyg', get_the_ID(), false ),
 		);
 
-	if ( isset( $entry['heading'] ) ) {
-
-    	$heading = esc_html( $entry['heading'] );
-	}
 	if ( isset( $entry['sub_heading'] ) ) {
 
     	$sub_heading = $entry['sub_heading'];
@@ -91,16 +183,34 @@ function tpcvendors_landing_section() {
 
     	$header_media = $entry['header_media'];
 	}
-	if ( isset( $entry['button_text'] ) ) {
-
-    	$button_text = esc_html( $entry['button_text'] );
-	}
 	?>
 	<div class="v-landing-image-bg" style="">
 		<div class="v-landing-wrap wrap full-height-section">
 
 			<?php tpcvendors_header_logo() ?>
 
+			<div class="row">
+				<?php tpcvendors_header_heading( 'medium' ); ?>
+				<div class="small-12 medium-6 columns">
+					<div class="header-media-content-wrap">
+           				<div class="header-media-content">
+							<?php echo $header_media; ?>
+						</div>
+					</div>	
+
+				</div>
+
+				<div class="small-12 medium-6 columns">
+
+					<?php tpcvendors_header_heading( 'large' ); ?>
+
+					<?php echo $sub_heading; ?>
+
+					<?php tpcvendors_button( 'vendor_header_button_text' ) ?>
+
+				</div>
+				
+			</div>
 		</div>
 	</div>	
 	<?php
@@ -113,6 +223,13 @@ function tpcvendors_landing_section() {
  * @todo  Section title
  */
 function tpcvendors_infobox_section() {
+
+	$section_title = get_post_meta( get_the_ID(), 'vendor_ib_heading_text', true );
+
+	if ( isset( $section_title ) ) {
+
+    	$section_title = esc_html( $section_title );
+    }
 
 	$entries = get_post_meta( get_the_ID(), '_tpcvendors_infoboxes_group', true );
 
@@ -142,6 +259,8 @@ function tpcvendors_infobox_section() {
 	    echo '<div class="v-info-box-section wrap">'; // Wrap Open
 	    echo '<div class="row" data-equalizer data-equalizer-mq="medium-up">'; // Row Open
 
+	    tpcvendors_section_title( $section_title );
+
 		foreach ( (array) $entries as $key => $entry ) {
 
 			// Store field values in variables to use in markup
@@ -166,7 +285,7 @@ function tpcvendors_infobox_section() {
 		    echo '<div class="icon-wrap' . $classes['icon_wrap'] . '"><span class="fa fa-' . $icon . '"></span></div>'; // Icon
 		    echo '<div class="box-inner' . $classes['box_inner'] . '">'; // Inner Box Open
 		    echo '<h3>' . $title . '</h3>'; // Title
-		    echo $content ; // Description
+		    echo '<p>' . $content . '</p>'; // Description
 		    echo '</div></div></div>'; // box-inner, info-box, and column Close
 		}
 
@@ -181,6 +300,13 @@ function tpcvendors_infobox_section() {
  * @todo  Section title
  */
 function tpcvendors_staff_section() {
+
+	$section_title = get_post_meta( get_the_ID(), 'vendor_staff_heading_text', true );
+
+	if ( isset( $section_title ) ) {
+
+    	$section_title = esc_html( $section_title );
+    }
 
 	$entries = get_post_meta( get_the_ID(), '_tpcvendors_staff_group', true );
 
@@ -200,6 +326,8 @@ function tpcvendors_staff_section() {
 	    echo '<div class="v-staff-section wrap">'; // Wrap Open
 	    echo '<div class="row">'; // Row Open
 
+	    tpcvendors_section_title( $section_title );
+
 		foreach ( (array) $entries as $key => $entry ) {
 
 			// Store field values in variables to use in markup
@@ -215,13 +343,14 @@ function tpcvendors_staff_section() {
 
 	        if ( isset( $entry['content'] ) ) {
 
-	        	$content = esc_html( $entry['content'] );
+	        	$content = tpcvendors_do_wysiwyg_output( $entry['content'] );
 	        }
 
 	        if ( isset( $entry['avatar_id'] ) ) {
 
 	        	$avatar = wp_get_attachment_image( $entry['avatar_id'], 'full' );
 	        }
+
 	        ?>
 			<div class="columns <?php echo $classes; ?>">
 				<div class="staff-profile">
@@ -282,7 +411,7 @@ function tpcvendors_extra_content_section() {
 
 	        if ( isset( $entry['content'] ) ) {
 
-	        	$content = esc_html( $entry['content'] );
+	        	$content = tpcvendors_do_wysiwyg_output( $entry['content'] );
 	        }
 
 	        echo '<div class="extra-content-wrap wrap">'; // Wrap Open
@@ -290,8 +419,30 @@ function tpcvendors_extra_content_section() {
 			echo '<div class="small-12 columns">';
 			echo '<h1 class="section-title">' . $section_title . '</h1>';
 			echo $content;
-			echo '</div></div></div>' // Wrap, Row, and Column Close
+			echo '</div></div></div>'; // Wrap, Row, and Column Close
 
 	    }
+	}
+}
+
+/**
+ * Call to Action Section Markup
+ *
+ * @return void
+ */
+function tpcvendors_final_cta() {
+
+	$entry = get_post_meta( get_the_ID(), 'vendor_header_button_text', true );
+
+	if ( isset( $entry ) ) {
+		?>
+		<div class="call-to-action cta-final-wrap wrap">
+		  	<div class="row">
+			    <div class="large-8 medium-9 medium-centered columns">
+			    	<?php tpcvendors_button( 'vendor_header_button_text' ); ?>
+			    </div>  
+		  	</div>
+		</div>
+		<?php
 	}
 }
